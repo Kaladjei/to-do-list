f0fdbc0399569fd26a508bb7eb899559d4a0c9eb
Kwame Larbi Adjei <kaladjei@gmail.com>
2018-08-05 01:43:32 +0000

    Add CSS

--- apps/basic-to-do-list/Main.hs
+++ apps/basic-to-do-list/Main.hs
@@ -6,22 +6,34 @@ module Main where
 
 import Miso
 import Miso.String (MisoString, ms)
-import qualified Miso.String as M
+import qualified Miso.String as S
 
 import Data.Bool (bool)
 import Data.IntMap (IntMap)
 import qualified Data.IntMap as Map
-import Data.Maybe (fromJust)
+import Data.Maybe (fromJust, fromMaybe)
+import Data.Monoid
 
-type Model = IntMap MisoString
+data Item = Item
+  { itemDescription :: MisoString
+  , editing         :: Bool
+  } deriving (Eq)
+
+data Model = Model
+  { allEntries   :: IntMap Item
+  , currentEntry :: Maybe (Map.Key, MisoString)
+  } deriving (Eq)
 
 initialModel :: Model
-initialModel = Map.singleton 1 "Enjoy life :)"
+initialModel =
+  Model (Map.singleton 1 $ Item "Enjoy life :)" False) Nothing
 
 data Action
   = NoOp
-  | Add (Map.Key, MisoString)
-  | New
+  | Edit Map.Key
+  | Input (Map.Key, MisoString)
+  | Add
+  | Delete
 
 main :: IO ()
 main = startApp App {..}
@@ -35,39 +47,89 @@ main = startApp App {..}
     mountPoint    = Nothing
 
 updateModel :: Action -> Model -> Effect Action Model
-updateModel New m = noEff $ Map.insert (nextKey m) "" m
-updateModel (Add kv) m = noEff $ (uncurry Map.insert kv) m
 updateModel NoOp m = noEff m
+updateModel (Edit k) m@Model{..} =
+  m { allEntries = enableEditing k (fromMaybe "" $ fmap snd currentEntry) allEntries
+    , currentEntry = Just (k, itemDescription $ allEntries Map.! k)
+    } <# do
+    focus $ "item-" <> S.ms k
+    pure NoOp
+updateModel (Input kv) m  = noEff m { currentEntry = Just kv }
+updateModel Add m@Model{..} =
+  let (k,desc) = fromJust currentEntry
+      (k', m') = updateEntry k desc m
+  in m' <# do
+    focus $ "item-" <> S.ms k'
+    pure NoOp
+updateModel Delete m@Model{..} = noEff $
+  deleteEntry (fst $ fromJust currentEntry) m
 
 nextKey :: IntMap a -> Int
-nextKey = fst . fst . fromJust . Map.maxViewWithKey
+nextKey = succ . fst . fst . fromJust . Map.maxViewWithKey
+
+enableEditing :: Map.Key -> MisoString -> IntMap Item -> IntMap Item
+enableEditing k desc = Map.foldrWithKey' go Map.empty
+  where
+    go k' item@Item{..} items
+      | k == k' = Map.insert k' (item { editing = True }) items
+      | editing && S.null desc = items
+      | editing = Map.insert k' (Item desc False) items
+      | otherwise = Map.insert k' item items
+
+updateEntry :: Map.Key -> MisoString -> Model -> (Map.Key, Model)
+updateEntry k desc m@Model{..} =
+  let items = Map.update (\item -> Just item { itemDescription = desc
+                                             , editing = False
+                                             }
+                         ) k allEntries
+      k' = nextKey items
+      newItem = Map.singleton k'  $ Item "" True
+  in ( k'
+     , m { allEntries = items <> newItem
+         , currentEntry = Nothing
+         }
+     )
+
+deleteEntry :: Map.Key -> Model -> Model
+deleteEntry k m@Model{..} =
+  let items = Map.delete k allEntries
+      m' = m { allEntries = items , currentEntry = Nothing }
+  in bool m' initialModel $ Map.null items
 
 viewModel :: Model -> View Action
-viewModel model = div_ [ id_ "contentWrapper" ]
+viewModel Model{..} = div_ [ id_ "contentWrapper" ]
   [ h1_ []
       [ text "Just "
       , em_ [] [ text "To Do" ]
       , text " It, Basically"
       ]
   , article_ [ class_ "post" ]
-      [ h2_ [] [ text "Enter and edit your list at will" ]
-      , p_ [] [ text lorem ]
-      ]
+      [ h2_ [] [ text "Enter and edit your list at will" ] ]
   , article_ []
-      [ ul_ [ id_ "theList", boolProp "contenteditable" True ] $
-          Map.foldrWithKey' (\k item xs -> entry k item : xs) [] model
+      [ div_ [ id_ "container" ]
+          [ ul_ [ id_ "theList" ] $
+              Map.foldrWithKey' (\k item@Item{..} xs ->
+                entry k (curDesc itemDescription) item : xs) [] allEntries
+          ]
       ]
   ]
+  where
+    curDesc desc = fromMaybe desc $ fmap snd currentEntry
 
-lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. \
-       \ Praesent non  mauris orci, quis tincidunt sapien. Vestibulum \
-       \ sodales est ut diam  tincidunt non bibendum dui cursus. Nulla \
-       \ blandit iaculis ipsum nec  ullamcorper. In mattis gravida magna \
-       \ et elementum."
-
-entry :: Map.Key -> MisoString -> View Action
-entry k item = li_
-  [ onInput (Add . (k,))
-  , onKeyDown (bool NoOp New . (== KeyCode 13))
+entry :: Map.Key -> MisoString -> Item -> View Action
+entry k desc Item{..} = li_
+  [ onClick (Edit k) ]
+  [ bool (text itemDescription) (itemInput k desc) editing
   ]
-  [ text item ]
+
+itemInput :: Map.Key -> MisoString -> View Action
+itemInput k desc = input_
+  [ id_ $ "item-" <> S.ms k
+  , autofocus_ True
+  , onInput (Input . (k,))
+  , value_ desc
+  , onKeyDown (bool NoOp (addOrDelete desc) . (== KeyCode 13))
+  ]
+
+addOrDelete :: MisoString -> Action
+addOrDelete = bool Add Delete . S.null
